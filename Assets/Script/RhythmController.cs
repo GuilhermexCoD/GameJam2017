5a59340e70b137a33eec3effb0473c26118c1002
dalsinster <ramonvmiranda@gmail.com>
2017-01-21 10:26:22 -0200

    Update Scripts
    Script de menu e Controle de Ritmo

---
+++ Assets/Script/RhythmController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhythmController : MonoBehaviour {
+
+	float timing, spaceTime, prevBeat;
+	public float timeSet, actionTime;
+	public bool action;
+
+	public GameObject wave;
+	public GameObject newWave;
+
+	void Awake ()
+	{
+		timing = timeSet;
+		prevBeat = timeSet - actionTime;
+		spaceTime = prevBeat;
+	}
+
+	void FixedUpdate () 
+	{
+		Debug.Log (action);
+		if (Time.fixedTime >= spaceTime)					//Marca o inicio do espaco pra acao do jogador...
+		{
+			action = true;
+		}
+		
+		if (Time.fixedTime >= timing) 						//Tempo final do hitmo pra chamada de funcoes...
+		{
+			timing = Time.fixedTime + timeSet;
+			spaceTime = Time.fixedTime + prevBeat;
+			action = false;
+			newWave = Instantiate (wave) as GameObject;
+			Destroy (newWave, 0.50f);
+		}
+
+	}
+}
---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public GameObject pauseMenu;
+
+	void Update ()
+	{
+//		float timeShow = Time.time;
+//		Debug.Log (timeShow);
+	}
+
+	public void PlayGame ()
+	{
+		SceneLoad (1);
+	}
+
+	public void PauseGame ()
+	{
+		pauseMenu.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	public void ResumeGame ()
+	{
+		Time.timeScale = 1;
+		pauseMenu.SetActive (false);
+	}
+
+	public void RestartGame ()
+	{
+		Time.timeScale = 1;
+		SceneLoad (1);
+	}
+
+	public void QuitGame ()
+	{
+		Time.timeScale = 1;
+		SceneLoad (0);
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit ();
+	}
+
+	public void Credits ()
+	{
+		SceneLoad (2);
+	}
+
+	void SceneLoad (int index)
+	{
+		SceneManager.LoadScene (index);
+	}
+}
