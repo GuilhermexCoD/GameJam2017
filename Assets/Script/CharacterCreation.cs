4cb6805b274c2f35b79b40d62d1ff25f8ef2b0f5
GuilhermexCoD <guilhermedevgames@gmail.com>
2017-01-21 14:04:20 -0200

    Player movimentando nas linhas

--- Assets/Script/CharacterCreation.cs
+++ Assets/Script/CharacterCreation.cs
@@ -3,25 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterCreation : MonoBehaviour {
-	public SpriteRenderer hairPart;
+	public SpriteRenderer decalPart;
 	public SpriteRenderer headPart;
+	public SpriteRenderer armPart;
+	public SpriteRenderer handPart;
 	public SpriteRenderer upperBodyPart;
-	public SpriteRenderer lowerBodyPart;
-
-	public List<Sprite> hair = new List<Sprite>();
+	public SpriteRenderer legLPart;
+	public SpriteRenderer legRPart;
+	public SpriteRenderer footLPart;
+	public SpriteRenderer footRPart;
+	public List<Sprite> decal = new List<Sprite>();
 	public List<Sprite> head = new List<Sprite>();
+	public List<Sprite> arm = new List<Sprite>();
 	public List<Sprite> upperBody = new List<Sprite>();
-	public List<Sprite> lowerBody = new List<Sprite>();
+	public List<Sprite> legL = new List<Sprite>();
+	public List<Sprite> legR = new List<Sprite>();
+
+	public List<Color> SkinColor = new List<Color> ();
 
 	// Use this for initialization
 	void Start () {
 
 		int rnd = Random.Range (0, head.Count);
 		headPart.sprite = head [rnd];
+		rnd = Random.Range (0, decal.Count);
+		decalPart.sprite = decal [rnd];
 		rnd = Random.Range (0, upperBody.Count);
 		upperBodyPart.sprite = upperBody [rnd];
-		rnd = Random.Range (0, lowerBody.Count);
-		lowerBodyPart.sprite = lowerBody [rnd];
+		rnd = Random.Range (0, legL.Count);
+		legLPart.sprite = legL [rnd];
+		legRPart.sprite = legR [rnd];
+
+		rnd = Random.Range (0, arm.Count);
+		armPart.sprite = arm [rnd];
+
+	
+
+
+
+		rnd = Random.Range (0, SkinColor.Count);
+		headPart.color = SkinColor [rnd];
+		armPart.color = SkinColor [rnd];
+		handPart.color = SkinColor [rnd];
+		footLPart.color = SkinColor [rnd];
+		footRPart.color = SkinColor [rnd];
 	}
 	
 	// Update is called once per frame
--- Assets/Script/RhythmController.cs
+++ Assets/Script/RhythmController.cs
@@ -11,16 +11,39 @@ public class RhythmController : MonoBehaviour {
 	public GameObject wave;
 	public GameObject newWave;
 
+	public List<int> music = new List<int> ();
+	public int musicSize;
+	public int activeLine;
+
+	public Vector3 targetPosition;
+
+	/// <summary>
+	/// The speed that the player change lines.
+	/// </summary>
+	public float speed;
+	/// <summary>
+	/// The waypoints of the lines.
+	/// </summary>
+	public List<GameObject> waypoints = new List<GameObject>();
 	void Awake ()
 	{
+		activeLine = 0;
+		if (waypoints.Count !=0) {
+			targetPosition = waypoints [0].transform.position;
+		}
+
 		timing = timeSet;
 		prevBeat = timeSet - actionTime;
 		spaceTime = prevBeat;
+
+		for (int i = 0; i < musicSize; i++) {
+			music.Add (Random.Range(0,3));
+		}
 	}
 
 	void FixedUpdate () 
 	{
-		Debug.Log (action);
+//		Debug.Log (action);
 		if (Time.fixedTime >= spaceTime)					//Marca o inicio do espaco pra acao do jogador...
 		{
 			action = true;
@@ -34,6 +57,20 @@ public class RhythmController : MonoBehaviour {
 			newWave = Instantiate (wave) as GameObject;
 			Destroy (newWave, 0.50f);
 		}
+
+		if (Input.GetKeyDown(KeyCode.UpArrow) && activeLine != 0) {
+			activeLine--;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow) && activeLine != 2) {
+			activeLine++;
+		}
+
+		targetPosition = waypoints [activeLine].transform.position;
+
+//		this.transform.position = Vector3.Lerp (this.transform.position, targetPosition, Time.fixedDeltaTime * 2);
+
+		this.transform.position =targetPosition;
+
 
 	}
 }
