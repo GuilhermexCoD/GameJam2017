90029b7b81367874bd3cb2ec82b12df982a77ca8
dalsinster <ramonvmiranda@gmail.com>
2017-01-22 04:05:21 -0200

    Audios - Vinicius

---
+++ Assets/Script/Audio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Audio : MonoBehaviour {
+
+	public AudioSource source;
+	public AudioClip BarulhoBotao;
+
+
+	public void OnHover()
+
+	{
+		source.PlayOneShot (BarulhoBotao);
+
+	}
+
+}
